8543c01e0f7c1519e8eb2aa72ef24859626d3998
pravin mane <122874987+pravinmane0@users.noreply.github.com>
2026-06-08 19:49:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,16 @@
-package com.driver;
+ppackage com.driver;
 
-public class Main {
+public class Product {
 
-  static class Product {
     public int product(int x, int y) {
-      return x*y;
+        return x * y;
     }
+
     public int product(int x, int y, int z) {
-      return x*y*z;
+        return x * y * z;
     }
+
     public double product(double x, double y) {
-      return x*y;
+        return x * y;
     }
-  }
-   public static void main(String[] args) {
-   Product p = new Product();
-     
-        System.out.println("Product of 2 numbers: " + p.product(5, 4));
-
-        System.out.println("Product of 3 numbers: " + p.product(5, 4, 2));
-
-        System.out.println("Product of 2 doubles: " + p.product(2.5, 4.0));
-   }
 }
